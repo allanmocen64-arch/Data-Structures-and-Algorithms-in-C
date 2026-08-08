08405291b7881c43be20e5bb789cf99a2b6ef114
allanmocen64 -arch <allanmocen64@gmail.com>
2026-08-08 22:32:42 +0300

    reversal arrays

---
+++ arrays_reversing.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int numbers[10] = {10, 20, 30, 40, 50};
+    int size = 5;
+    int left = 0;
+    int right = size - 1;
+    int temp = numbers[left];
+
+    while (left < right)
+    {
+        int temp = numbers[left];
+        numbers[left] = numbers[right];
+        numbers[right] = temp;
+        left++;
+        right--;
+    }
+    printf("Reversed Array: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d", numbers[i]);
+    }
+
+    return 0;
+}
