8a759c3d57b263b6f58985c84917426b49c5f8bf
allanmocen64 -arch <allanmocen64@gmail.com>
2026-08-13 10:22:34 +0300

    Update on Binary search

--- Arrays/arrays_BinarySearch.c
+++ Arrays/arrays_BinarySearch.c
@@ -3,19 +3,23 @@
 int main()
 {
 
-    int numbers[9] = {1, 3, 5, 7, 9, 13, 15, 17};
+    int numbers[9] = {1, 3, 5, 7, 9, 11, 13, 15, 17};
     int size = 9;
-    int target = 15;
+    int target;
 
     int left = 0;
     int right = size - 1;
+    int found = -1;
+
+    printf("Enter a number to search: ");
+    scanf("%d", &target);
 
     while (left <= right)
     {
         int middle = (left + right) / 2;
         if (numbers[middle] == target)
         {
-            printf("Found at index %d\n", middle);
+            found = middle;
             break;
         }
         else if (target > numbers[middle])
@@ -26,7 +30,15 @@ int main()
         {
             left = middle - 1;
         }
-        }
+    }
+    if (found != -1)
+    {
+        printf("Found at index %d\n", found);
+    }
+    else
+    {
+        printf("Not found!");
+    }
 
     return 0;
 }
