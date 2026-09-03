344c25c7690f062f11678711f6ad7d7786957825
allanmocen64 -arch <allanmocen64@gmail.com>
2026-09-03 08:12:06 +0300

    Quick sort but incomplete

---
+++ Searching Algorithms/Quick_sort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;
+}
+void quicksort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivotIndex = partition(arr, low, high);
+        quicksort(arr, low, pivotIndex - 1);
+        quicksort(arr, pivotIndex + 1, high);
+    }
+}
+
+int main()
+{
+
+    return 0;
+}
